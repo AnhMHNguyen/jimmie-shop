0012a183513965f64c59453284112c4ea0149305
AnhMHNguyen <84159499+AnhMHNguyen@users.noreply.github.com>
2021-09-28 01:19:57 -0700

    initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -199,7 +199,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
     
 MEDIA_ROOT = 'static/images'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
