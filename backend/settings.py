ff75090fabc5335f73648520f5f6111f3ed7fdb0
AnhMHNguyen <84159499+AnhMHNguyen@users.noreply.github.com>
2021-09-27 17:31:40 -0700

    initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -154,7 +154,7 @@ if DEBUG :
     }
 else:  
     DATABASES = {
-        'default': dj_database_url.config()
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
 
 
