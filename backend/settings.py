3dcd69847b5bd90f8b003c48e70e29997693aec8
AnhMHNguyen <84159499+AnhMHNguyen@users.noreply.github.com>
2021-09-28 18:01:36 -0700

    initial commit

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-5*@dozk=!=gp6qau-j-l0ql#^shy*)!)*5==s=a+4xx9^2&6$z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
